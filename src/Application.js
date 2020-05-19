b4f647cdc5912780c7fcc09d014b2210f92cdb6c
“drtoil” <axmed13@hotmail.com>
2020-05-19 21:27:56 +0100

    added reset button on the state

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import HighScore from './HighScore';
 
 class Application extends Component {
@@ -6,24 +6,40 @@ class Application extends Component {
     super(props);
     this.state = {
       count: 0,
-      overTen: false
+      overTen: false,
     };
   }
   handleClick = () => {
-      this.setState({count:this.state.count + 1});
+    this.setState({ count: this.state.count + 1 });
+  };
+  componentDidUpdate(props, state) {
+    if (
+      this.state.count > 10 &&
+      this.state.count !== state.count &&
+      !this.state.overTen
+    ) {
+      console.log('Updating over ten');
+      this.setState({ overTen: true });
+    }
   }
-  componentDidUpdate(props, state){
-      if(this.state.count > 10 && this.state.count !== state.count && !this.state.overTen){
-        console.log('Updating over ten');
-        this.setState({overTen: true});
-      }
+
+  resetCount = (e) => {
+    console.log('Event is', e);
+    this.setState({
+      count: 0,
+      overTen: false
+    });
   }
+
   render() {
     let { count } = this.state;
     return (
       <div>
         <h1> You clicked the button {count} times</h1>
-        <HighScore />
+        <HighScore
+          overTen={this.state.overTen}
+          onReset={this.resetCount}
+        />
         <span>
           <button onClick={() => this.handleClick()}>Click me</button>
         </span>
